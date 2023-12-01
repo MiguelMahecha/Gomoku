1080ce830f897553a4502f6b6e3f933e971183c5
MiguelMahecha <118125049+MiguelMahecha@users.noreply.github.com>
2023-11-30 19:35:10 -0500

    First Commit

---
+++ src/presentation/GomokuGUI.java
@@ -0,0 +1,9 @@
+package presentation;
+
+import javax.swing.*;
+
+public class GomokuGUI extends JFrame {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
