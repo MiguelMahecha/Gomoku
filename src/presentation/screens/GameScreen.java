cf11a6af66ed4c882a86e6318d0ff2b960d6a440
MiguelMahecha <118125049+MiguelMahecha@users.noreply.github.com>
2023-12-02 15:29:33 -0500

    fix(presentation): Board is drawing correctly now (thanks to our monitor).

--- src/presentation/screens/GameScreen.java
+++ src/presentation/screens/GameScreen.java
@@ -8,15 +8,16 @@ import java.awt.*;
 
 public class GameScreen extends JPanel {
     private final Gomoku gomoku;
-//    private GridBagConstraints gbc;
     private JPanel board;
     private JPanel gameInfo;
     private final JButton[][] buttons;
     private Dimension frameDimension;
+    private GridBagConstraints gbc;
 
     public GameScreen(Gomoku gomoku, Dimension dimension) {
         this.gomoku = gomoku;
-        setLayout(new BorderLayout(gomoku.getBoard().length, gomoku.getBoard().length));
+        setLayout(new GridBagLayout());
+        this.gbc = new GridBagConstraints();
         buttons = new JButton[gomoku.getBoard().length][gomoku.getBoard().length];
         this.frameDimension = dimension;
         prepareElements();
@@ -28,36 +29,42 @@ public class GameScreen extends JPanel {
     }
 
     private void prepareElementsBoard() {
-        setLayout(new BorderLayout(gomoku.getBoard().length, gomoku.getBoard().length));
+//        setLayout(new BorderLayout(gomoku.getBoard().length, gomoku.getBoard().length));
         board = new JPanel();
-        board.setLayout(new GridLayout(gomoku.getBoard().length, gomoku.getBoard().length));
+        board.setLayout(new GridBagLayout());
         board.setSize(new Dimension((int)frameDimension.getWidth() / 2, (int)frameDimension.getHeight()));
-
+        GridBagConstraints gbc = new GridBagConstraints();
         for (int i = 0; i < gomoku.getBoard().length; i++) {
             for (int j = 0; j < gomoku.getBoard().length; j++) {
                 JButton button = new JButton();
                 int tileWidth = board.getWidth() / gomoku.getBoard().length;
                 int tileHeight = board.getHeight() / gomoku.getBoard().length;
                 int tileDim = Math.min(tileHeight, tileWidth);
-                button.setPreferredSize(new Dimension(tileDim, tileDim));
+                button.setSize(new Dimension(tileDim, tileDim));
                 button.setBackground(Color.BLUE);
+                int preferredSize = Math.min(board.getWidth() / gomoku.getBoard().length, board.getHeight() / gomoku.getBoard().length);
+                gbc.gridx = j;
+                gbc.gridy = i;
+                gbc.fill = GridBagConstraints.BOTH;
+                gbc.weightx = 1.0;
+                gbc.weighty = 1.0;
+                gbc.insets = new Insets(2, 2, 2, 2);
+                button.setPreferredSize(new Dimension(preferredSize, preferredSize));
                 buttons[i][j] = button;
-                board.add(button);
+                board.add(button, gbc);
             }
         }
         board.setVisible(true);
-        add(board);
+        this.gbc.gridx = 0;
+        this.gbc.weightx = 0.7;
+        add(board, this.gbc);
     }
 
     private void prepareElementsGameInfo() {
         gameInfo = new JPanel();
         gameInfo.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
-        gbc.anchor = GridBagConstraints.WEST;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.insets = new Insets(5, 5, 5, 5);
-
-        gbc.gridx = 0;
+        gbc.insets = new Insets(1, 1, 1, 1);
 
         JLabel playerOne = new JLabel(String.format("%s", gomoku.getPlayerOne().name()));
         gbc.gridy = 0;
@@ -65,7 +72,10 @@ public class GameScreen extends JPanel {
         JLabel playerTwo = new JLabel(String.format("%s", gomoku.getPlayerTwo().name()));
         gbc.gridy = 1;
         gameInfo.add(playerTwo, gbc);
+
         gameInfo.setVisible(true);
-        add(gameInfo);
+        this.gbc.gridx = 1;
+        this.gbc.weightx = 0.3;
+        add(gameInfo, this.gbc);
     }
 }
